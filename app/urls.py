7ee6335f72b686ada312dbe166833d984e872fbd
ccbn <thanhtin@siliconstraits.vn>
2016-06-19 00:36:44 +0700

    add api init parcel from json

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from . import views_task
+from . import views_task, views_parcel
 
 app_name = 'api'
 
 urlpatterns = [
     url(r'^task/$', views_task.TaskList.as_view()),
+    url(r'^parcel/$', views_parcel.ParcelList.as_view()),
 ]
