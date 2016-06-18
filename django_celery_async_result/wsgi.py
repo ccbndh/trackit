327df5d57755b26d5b2bdd15ef7fc1a31250ac17
ccbn <thanhtin@siliconstraits.vn>
2016-06-18 13:06:35 +0700

    rename from public repo django_celery_async_result

--- django_celery_async_result/wsgi.py
+++ django_celery_async_result/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_celery_async_result project.
+WSGI config for trackit project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_celery_async_result.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trackit.settings")
 
 application = get_wsgi_application()
