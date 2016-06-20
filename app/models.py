b2cffb52ae6d366ccac6db0f3f8a5d0c70be70e9
ccbn <thanhtin@siliconstraits.vn>
2016-06-20 11:40:58 +0700

    init new model architech

--- app/models.py
+++ app/models.py
@@ -1,50 +1,22 @@
 from __future__ import unicode_literals
-
-from collections import namedtuple
-
+from django.utils import timezone
 from django.contrib.postgres.fields import JSONField, ArrayField
 from django.db import models
-from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
-
-PARCEL_STATUS_STRUCT = namedtuple("STRUCT_PARCEL_STATUS",
-                                  "AMBIGUOUS_TRACK_ID NOT_FOUND ACTIVE EXEMPTED DELAYED DELIVERED")
-PARCEL_STATUS = PARCEL_STATUS_STRUCT(AMBIGUOUS_TRACK_ID="ambiguous_track_id", NOT_FOUND="not_found", ACTIVE="active",
-                                     EXEMPTED="exempted", DELAYED="delayed", DELIVERED="delivered")
-
-ANALYTICS_FIELDS = (
-    ("regular", "Regular"),
-    ("exemption", "Exemption"),
-    ("delay", "Delay")
-)
-
-MAPPING_STATUS = (
-    ("mapped", "Mapped"),
-    ("not_sure", "Not Sure"),
-    ("unmapped", "Unmapped")
-)
-
-TIMEZONE_AWARENESS = (
-    ("utc", "UTC"),
-    ("destination_local_time", "Destination Local Time"),
-    ("origin_local_time", "Origin Local Time"),
-    ("location_local_time", "Location Local Time")
-)
 
 
 @python_2_unicode_compatible  # only if you need to support Python 2
-class CarrierMasterData(models.Model):
-    class Meta:
-        db_table = "carrier"
+class Carrier(models.Model):
+    name = models.CharField(max_length=500, null=True, blank=True)
+    slug_name = models.CharField(max_length=500, null=True, blank=True)
 
-    name = models.CharField(max_length=500, null=True)
-    slug_name = models.CharField(max_length=500, null=True, unique=True)
     carrier_id = models.CharField(max_length=100, unique=True)
+    carrier_language = models.CharField(max_length=100, null=True, blank=True)
+    carrier_cs_phone = models.CharField(max_length=200, null=True, blank=True)
     carrier_url = models.CharField(max_length=200, null=True, blank=True)
     carrier_url_tracking = models.CharField(max_length=200, null=True, blank=True)
     carrier_logo = models.CharField(max_length=100, null=True, blank=True)
-    comments = models.CharField(max_length=200, null=True, blank=True)
-    pattern_regex = ArrayField(models.CharField(max_length=500), null=True, blank=True)
+    pattern_regex = ArrayField(models.CharField(max_length=1000), null=True, blank=True)
 
     def __str__(self):
         return unicode(self.name)
@@ -52,12 +24,7 @@ class CarrierMasterData(models.Model):
 
 @python_2_unicode_compatible  # only if you need to support Python 2
 class Parcel(models.Model):
-    class Meta:
-        db_table = "parcel"
-        unique_together = ('carrier', 'parcel_id')
-
     parcel_id = models.CharField(max_length=500, null=True)
-    carrier = models.ForeignKey(CarrierMasterData, db_constraint=False, null=True)
     picked_up_date = models.DateTimeField(null=True, blank=True)
     delivered_date = models.DateTimeField(null=True, blank=True)
     imported_date = models.DateTimeField(default=timezone.now)
@@ -71,37 +38,19 @@ class Parcel(models.Model):
     destination_locality = models.CharField(max_length=500, null=True, blank=True)
     destination_raw_location = models.CharField(max_length=500, null=True, blank=True)
 
-    origin_json_blob = JSONField(null=True, blank=True)
-
-    """
-    Statuses:
-    - ambiguous_track_id: Parcel ID matches with pattern of multiple carriers, no carrier has been chosen yet
-    - not_found: Parcel ID matches with only one carrier, or a specific carrier was chosen, no data returned from scraper
-    - active: Parcel ID matches with only one carrier, or a specific carrier was chosen, found data from scraper
-    - exempted: active parcel, last event is `final` and analytics type is `exemption`
-    - delayed: active parcel, last event is `final` and analytics type is `delay`
-    - delivered: active parcel, last event is `final` and analytics type is `regular`
-    """
-    status = models.CharField(max_length=100, default='not_found')
-
     def __str__(self):
         return unicode(self.parcel_id)
 
 
 @python_2_unicode_compatible  # only if you need to support Python 2
 class Event(models.Model):
-    class Meta:
-        db_table = "event"
-        unique_together = ('parcel', 'original_event_type',
-                           'parsed_event_time', 'original_location')
+    carrier = models.ManyToManyField(Carrier)
+    parcel = models.ForeignKey(Parcel)
 
-    parcel = models.ForeignKey(Parcel, db_constraint=False)
-    carrier = models.ForeignKey(CarrierMasterData, null=True, db_constraint=False)
-    original_event_type = models.CharField(max_length=500, null=True, blank=True)
-    original_time = models.CharField(max_length=500, null=True, blank=True)
-    original_location = models.CharField(max_length=500, null=True, blank=True)
-    additional_params = JSONField(null=True, blank=True)
-    parsed_event_time = models.DateTimeField(null=True)
+    original_event_type = models.CharField(max_length=500, null=True)
+    original_time = models.CharField(max_length=500, null=True)
+    original_location = models.CharField(max_length=500, null=True)
+    additional_params = JSONField(null=True)
 
     def __str__(self):
         return unicode(self.original_event_type)
