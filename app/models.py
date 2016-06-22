d033741c9115d2149a58a23eac20f213416846a9
ccbn <thanhtin@siliconstraits.vn>
2016-06-22 10:13:28 +0700

    init model, foreignkey

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 
-from django.contrib.postgres.fields import JSONField
+from django.contrib.postgres.fields import JSONField, ArrayField
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
@@ -12,3 +12,49 @@ class RawData(models.Model):
 
     def __str__(self):
         return unicode(self.parcel_id)
+
+
+@python_2_unicode_compatible  # only if you need to support Python 2
+class Carrier(models.Model):
+    name = models.CharField(max_length=500, null=True, blank=True)
+    slug_name = models.CharField(max_length=500, null=True, blank=True)
+    carrier_id = models.CharField(max_length=100, unique=True)
+    carrier_language = models.CharField(max_length=100, null=True, blank=True)
+    carrier_cs_phone = models.CharField(max_length=200, null=True, blank=True)
+    carrier_url = models.CharField(max_length=200, null=True, blank=True)
+    carrier_url_tracking = models.CharField(max_length=200, null=True, blank=True)
+    carrier_support_languages = ArrayField(models.CharField(max_length=50), null=True, blank=True)
+    carrier_logo = models.CharField(max_length=100, null=True, blank=True)
+    comments = models.CharField(max_length=200, null=True, blank=True)
+
+    pattern_regex = ArrayField(models.CharField(max_length=1000), null=True, blank=True)
+
+    def __str__(self):
+        return unicode(self.name)
+
+
+@python_2_unicode_compatible  # only if you need to support Python 2
+class Parcel(models.Model):
+    parcel_id = models.CharField(max_length=200, null=True)
+    status = models.CharField(max_length=100, null=True)
+    weight = models.CharField(max_length=100, null=True)
+    size = models.CharField(max_length=100, null=True)
+    price = models.CharField(max_length=100, null=True)
+    deliver_time = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return unicode(self.parcel_id)
+
+
+@python_2_unicode_compatible  # only if you need to support Python 2
+class Event(models.Model):
+    parcel = models.ForeignKey(Parcel)
+    carrier = models.ForeignKey(Carrier, null=True)
+    original_event_type = models.CharField(max_length=500, null=True, blank=True)
+    original_time = models.CharField(max_length=500, null=True, blank=True)
+    original_location = models.CharField(max_length=500, null=True, blank=True)
+    additional_params = JSONField(null=True, blank=True)
+    parsed_event_time = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return unicode(self.original_event_type)
