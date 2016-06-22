ad37db251dc64e17e02d3d776a865e2af41834bf
ccbn <thanhtin@siliconstraits.vn>
2016-06-22 12:33:30 +0700

    add serializer model

---
+++ app/serializers.py
@@ -0,0 +1,23 @@
+import logging
+
+from rest_framework import serializers
+
+from .models import Event, Parcel, Carrier
+
+# Get an instance of a logger
+logger = logging.getLogger("api.activity")
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+
+
+class ParcelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Parcel
+
+
+class CarrierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Carrier
