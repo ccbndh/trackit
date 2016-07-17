64da4ad62694ff7e967069d8a9e81ae52d5d49f2
ccbn <tin.nguyenthanh@tiki.vn>
2016-07-18 00:03:08 +0700

    add form enter parcel id

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,26 +5,70 @@ var Route = require('react-router').Route
 var Link = require('react-router').Link
 var browserHistory = require('react-router').browserHistory
 
-var Home = React.createClass({
-	render: function() {
-		return (
-			<h1>
-			Tracking your parcel!
-			</h1>
-		)
-	}
+var Welcome = React.createClass({
+    render: function () {
+        return (
+            <h1>
+                Tracking your parcel!
+            </h1>
+        )
+    }
 });
 
+var InputForm = React.createClass({
+    getInitialState: function () {
+        return {parcelId: ''};
+    },
+    handleParcelIdChange: function (e) {
+        this.setState({parcelId: e.target.value});
+    },
+    handleSubmit: function (e) {
+        e.preventDefault();
+        var parcelId = this.state.parcelId.trim();
+        if (!parcelId) {
+            return;
+        }
+        // TODO: send request to the server
+        this.setState({parcelId: ''});
+    },
+    render: function () {
+        return (
+            <form className="input-group" onSubmit={this.handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Enter your parcel id"
+                    value={this.state.parcelId}
+                    onChange={this.handleParcelIdChange}
+                />
+                <input type="submit" value="Track"/>
+            </form>
+        );
+    }
+});
+
+
+var Home = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <Welcome />
+                <InputForm />
+            </div>
+        )
+    }
+});
+
+
 var Tracking = React.createClass({
-  render: function() {
-     return (<h1>Tracking result: {this.props.params.parcelId}</h1>);
-  }
+    render: function () {
+        return (<h1>Tracking result: {this.props.params.parcelId}</h1>);
+    }
 });
 
 ReactDOM.render((
-  <Router history={browserHistory}>
-      <Route path="/" component={Home} />
-      <Route path="/:parcelId" component={Tracking} />
-  </Router>
+    <Router history={browserHistory}>
+        <Route path="/" component={Home}/>
+        <Route path="/:parcelId" component={Tracking}/>
+    </Router>
 
 ), document.getElementById('container'));
