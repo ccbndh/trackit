394fe70827267844f055a5cfb4513ff0437f8ac9
ccbn <tin.nguyenthanh@tiki.vn>
2016-07-20 09:16:18 +0700

    fix on false test case, must be use decorator override_settings test case

--- app/tests.py
+++ app/tests.py
@@ -15,59 +15,66 @@ from app.serializers import ParcelSerializer, CarrierSerializer, EventNestedSeri
 class AddTestCase(TestCase):
     fixtures = ['app/data/carriers.json']
 
-    def tearDown(self):
-        time.sleep(3)  # sleep time in seconds
+
+    # def test_model(self):
+    #     # TODO Only test syntax, must be assertEqual in this test case
+    #
+    #     parcel_id = 'EL745355158VN'
+    #     carrier = carrier_identify(parcel_id)
+    #     if carrier:
+    #         spider = eval(CARRIER_SPIDER[carrier.slug_name])(parcel_id, carrier.slug_name)
+    #     else:
+    #         spider = eval('GHNSpider')(parcel_id, 'ghn')
+    #     result = spider.normalize()
+    #
+    #     # create or update parcel by parcel_id
+    #     parcel = result.get('parcel')
+    #     parcel_serializer = ParcelSerializer(data=parcel, partial=True)  # normalize data from spider
+    #     if parcel_serializer.is_valid():
+    #         parcel_obj, _ = Parcel.objects.update_or_create(parcel_id=parcel.get('parcel_id'), defaults=parcel_serializer.data)
+    #     else:
+    #         parcel_obj = Parcel.objects.get(parcel_id=parcel.get('parcel_id'))
+    #
+    #     # get carrier
+    #     carrier = result.get('carrier')
+    #     carrier_obj = Carrier.objects.get(slug_name=carrier.get('slug_name'))
+    #
+    #     # get or update event
+    #     events = result.get('events_details')
+    #     event_obj_list = []
+    #     for event in events:
+    #         # can't use here because .data always valid and missing parcel
+    #         # event_serializer = EventSerializer(data=event, partial=True)
+    #         event_obj, _ = Event.objects.get_or_create(parcel=parcel_obj, carrier=carrier_obj, **event)
+    #         event_obj_list.append(event_obj)
+    #     events_serializer = EventNestedSerializer(event_obj_list, many=True)
+    #
+    #     # TODO how to use with events (many=True)
+    #     '''
+    #         events_serializer = EventSerializer(data=events, many=True, partial=True)
+    #         events_serializer.is_valid()
+    #     '''
+
+    def test_carrier_identify(self):
+        parcel_id = 'EL745355158VN'
+        carrier = carrier_identify(parcel_id)
+        self.assertEqual(carrier.slug_name, 'vnpost', "Should return true parcel_id")
 
     @override_settings(
         CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
         CELERY_ALWAYS_EAGER=True,
         BROKER_BACKEND='memory')
-    def test_model(self):
-        # TODO Only test syntax, must be assertEqual in this test case
-
-        parcel_id = 'EL745355158VN'
-        carrier = carrier_identify(parcel_id)
-        if carrier:
-            spider = eval(CARRIER_SPIDER[carrier.slug_name])(parcel_id, carrier.slug_name)
-        else:
-            spider = eval('GHNSpider')(parcel_id, 'ghn')
-        result = spider.normalize()
-
-        # create or update parcel by parcel_id
-        parcel = result.get('parcel')
-        parcel_serializer = ParcelSerializer(data=parcel, partial=True)  # normalize data from spider
-        if parcel_serializer.is_valid():
-            parcel_obj, _ = Parcel.objects.update_or_create(parcel_id=parcel.get('parcel_id'), defaults=parcel_serializer.data)
-        else:
-            parcel_obj = Parcel.objects.get(parcel_id=parcel.get('parcel_id'))
-
-        # get carrier
-        carrier = result.get('carrier')
-        carrier_obj = Carrier.objects.get(slug_name=carrier.get('slug_name'))
-
-        # get or update event
-        events = result.get('events_details')
-        event_obj_list = []
-        for event in events:
-            # can't use here because .data always valid and missing parcel
-            # event_serializer = EventSerializer(data=event, partial=True)
-            event_obj, _ = Event.objects.get_or_create(parcel=parcel_obj, carrier=carrier_obj, **event)
-            event_obj_list.append(event_obj)
-        events_serializer = EventNestedSerializer(event_obj_list, many=True)
-
-        # TODO how to use with events (many=True)
-        '''
-            events_serializer = EventSerializer(data=events, many=True, partial=True)
-            events_serializer.is_valid()
-        '''
-
     def test_add_task(self):
         parcel_id = 'EL745355158VN'
         result = task_get_data_from_spider.delay(parcel_id)
         self.assertTrue(result.successful())
-        parcel_id_res = result.result.get('parcel').get('parcel_id')
+        self.assertEqual(len(result.result), 11, "Should return 11 list event")
+        parcel_id_res = result.result[0].get('parcel').get('parcel_id')
         self.assertEqual(parcel_id, parcel_id_res, "Should return true parcel_id")
 
+        # on enter parcel_id 2nd
         result = task_get_data_from_spider.delay(parcel_id)
-        parcel_id_res = result.result.get('parcel').get('parcel_id')
-        self.assertEqual(parcel_id, parcel_id_res, "Should return true parcel_id 2 times")
+        self.assertTrue(result.successful())
+        self.assertEqual(len(result.result), 11, "Should return 11 list event")
+        parcel_id_res = result.result[0].get('parcel').get('parcel_id')
+        self.assertEqual(parcel_id, parcel_id_res, "Should return true parcel_id")
