c41ebc78507215283d8659abb94831f2a148e00d
ccbn <thanhtinpk092007@gmail.com>
2016-07-23 12:32:09 +0700

    register model with admin interface

--- app/admin.py
+++ app/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Carrier, Parcel, Event
+
+admin.site.register(Carrier)
+admin.site.register(Parcel)
+admin.site.register(Event)
