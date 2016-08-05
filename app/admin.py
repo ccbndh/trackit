b1595ff836bd5457b78610786d615d3050ef3fee
ccbn <thanhtinpk092007@gmail.com>
2016-08-05 20:06:50 +0700

    show event belong to Parcel in admin page

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 
 from .models import Carrier, Parcel, Event
 
+
+class EventInline(admin.StackedInline):
+    fields = ('event_name', 'event_time', 'event_location', 'delivery_status')
+    model = Event
+
+
+class ParcelAdmin(admin.ModelAdmin):
+    inlines = [
+        EventInline,
+    ]
+
+
 admin.site.register(Carrier)
-admin.site.register(Parcel)
+admin.site.register(Parcel, ParcelAdmin)
 admin.site.register(Event)
